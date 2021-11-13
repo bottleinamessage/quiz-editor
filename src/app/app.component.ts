58e2cfe4536ff5a911b84b4d905a3165cb4f9564
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-11-13 13:40:22 -0600

    Handle endpoint errors better ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,8 @@ export class AppComponent implements OnInit {
   ) {
   }
 
+  errorLoadingQuizzes = false;
+
   ngOnInit() {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
@@ -40,7 +42,8 @@ export class AppComponent implements OnInit {
         }));
       }
       , err => {
-        console.error(err);
+        console.error(err.error);
+        this.errorLoadingQuizzes = true;
       }
     );
 

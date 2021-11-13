e8c37468b6c81006ea990eea0baeacd44e74bbbb
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-11-13 15:35:24 -0600

    My #week-12-slack-n-tell

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,7 @@ export class AppComponent implements OnInit {
   ) {
   }
 
+  loading = true;
   errorLoadingQuizzes = false;
 
   loadQuizzesFromCloud = async () => {
@@ -39,10 +40,13 @@ export class AppComponent implements OnInit {
         }))
         , markedForDelete: false
       }));      
+
+      this.loading = false;
     }
     catch (err) {
       console.error(err);
-      this.errorLoadingQuizzes = true;      
+      this.errorLoadingQuizzes = true;
+      this.loading = false;      
     }
   };
 

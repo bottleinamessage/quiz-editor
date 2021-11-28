d0b40bf01853e2dc4b6fa9011dca3afdfe41d52f
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-11-28 12:20:58 -0600

    Got saving of edited quizzes working...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService, QuizFromWeb } from './quiz.service';
+import { QuizService, QuizFromWeb, ShapeForSavingNewQuizzes, ShapeForSavingEditedQuizzes } from './quiz.service';
 
 import {
   trigger
@@ -230,5 +230,30 @@ export class AppComponent implements OnInit {
 
   detailsFromLeftAnimationDone = () => {
     this.detailsFromLeftAnimationState = "leftPosition";
+  };
+
+  saveQuizzes = async () => {
+    try {
+
+        const newQuizzes: ShapeForSavingNewQuizzes[] = [];
+
+        const editedQuizzes: ShapeForSavingEditedQuizzes[] = this.getEditedQuizzes().map(x => ({
+          quiz: x.quizName
+          , questions: x.quizQuestions.map(y => ({
+            question: y.questionName
+          }))
+        }));
+
+        const numberOfUpdatedQuizzes = await this.quizSvc.saveQuizzes(
+          editedQuizzes
+          , newQuizzes
+        );
+
+        console.log("numberOfUpdatedQuizzes", numberOfUpdatedQuizzes);
+    }
+
+    catch (err) {
+      console.error(err);
+    }
   };
 }

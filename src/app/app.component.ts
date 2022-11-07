2b1db37be6da5cac60564c9531ef20eaa5d6c499
Anthony Rice <arice@madisoncollege.edu>
2022-11-06 21:10:18 -0600

    Selecting a quiz and keeping track of it
    Boom ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,4 +39,11 @@ export class AppComponent implements OnInit {
 
   quizzes: QuizDisplay[] = [];
 
+  selectedQuiz: QuizDisplay | undefined = undefined
+
+  selectQuiz = (q: QuizDisplay) => {
+    this.selectedQuiz = q;
+    console.log(this.selectedQuiz);
+  };
+
 }

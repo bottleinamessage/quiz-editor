c8d7aae605e95b4a1c19fff884b76918ef32bd6e
tsteelematc <tsteele@madisoncollege.edu>
2022-11-12 09:45:42 -0600

    Ready to use the HttpClient service from Angular...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 interface QuizFromWeb {
@@ -12,7 +13,9 @@ interface QuizFromWeb {
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(
+    private angularHttpClient: HttpClient
+  ) { }
 
   loadQuizzes = () => {
 

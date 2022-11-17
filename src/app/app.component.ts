f1f429464b0db6c9612d8c60d445de848f2d9458
DrewHalverson <67759475+DrewHalverson@users.noreply.github.com>
2022-11-17 11:30:23 -0600

    Adventures with async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -91,4 +91,60 @@ export class AppComponent implements OnInit {
       this.selectedQuiz.quizQuestions = this.selectedQuiz.quizQuestions.filter(x => x !== questionToRemove);
     }
   };
+
+
+  jsPromiseOne = () => {
+    const n = this.quizSvc.getMagicNumber(true);
+    console.log(n);
+
+    n.then(
+      number => {
+        console.log(number);
+        const n2 = this.quizSvc.getMagicNumber(true);
+        console.log(n2);
+
+        n2.then(x => console.log(x)).catch(e => console.error(e));
+      }
+    ).catch(
+      err => {
+        console.error(err);
+      }
+    )
+  };
+
+  jsPromiseTwo = async () => {
+
+    try {
+    const x = await this.quizSvc.getMagicNumber(true);
+    console.log(x);
+
+    const y = await this.quizSvc.getMagicNumber(true);
+    console.log(y);
+    }
+
+    catch (err) {
+      console.error(err);
+    }
+  };
+
+  jsPromiseThree = async () => {
+
+    try {
+    const x = this.quizSvc.getMagicNumber(true);
+    console.log(x);
+
+    const y = this.quizSvc.getMagicNumber(true);
+    console.log(y);
+
+    //const results = await Promise.all([x, y]);
+    const results = await Promise.race([x, y]);
+    console.log(results);
+    }
+
+    catch (err) {
+      console.error(err);
+    }
+  };
+  
+
 }

39c0d2e7518868f3160e545f8296044b10e19693
DrewHalverson <67759475+DrewHalverson@users.noreply.github.com>
2022-11-17 10:14:14 -0600

    set up error handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,9 @@ export class AppComponent implements OnInit {
   ) {
   }
 
+  errorLoadingQuizzes = false;
+
+
   ngOnInit() {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
@@ -40,7 +43,8 @@ export class AppComponent implements OnInit {
         }));
       }
       , err => {
-        console.error(err);
+        console.error(err.error);
+        this.errorLoadingQuizzes = true;
       }
     );
   }

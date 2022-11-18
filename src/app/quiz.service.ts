fcc46d05d6b86caff4ee9f04b04371ec5f227eff
Anna McCreery <114881364+annamccreery@users.noreply.github.com>
2022-11-18 11:06:27 -0600

    updated from observable tp promises

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -21,7 +21,7 @@ export class QuizService {
 
     const quizzesFromWeb = this.angularHttpClient.get<QuizFromWeb[]>(
       "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
-    );
+    ).toPromise();
 
     return quizzesFromWeb;
   };

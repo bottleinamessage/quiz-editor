c592159e0c0c5668f9e4edf3232619aaa78bba1b
Anna McCreery <114881364+annamccreery@users.noreply.github.com>
2022-11-18 11:11:21 -0600

    imported lastValueFrom and applied
    rather than the deprecated toPromise

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from'@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { lastValueFrom } from 'rxjs';
 interface QuizFromWeb {
   name: string;
   questions: {
@@ -19,9 +19,10 @@ export class QuizService {
 
   loadQuizzes = () => {
 
-    const quizzesFromWeb = this.angularHttpClient.get<QuizFromWeb[]>(
+    const quizzesFromWeb = lastValueFrom(
+      this.angularHttpClient.get<QuizFromWeb[]>(
       "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
-    ).toPromise();
+    ));
 
     return quizzesFromWeb;
   };

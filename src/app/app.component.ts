a0550f644c555d08db44558448ac9f4016c24d77
tsteelematc <tsteele@madisoncollege.edu>
2022-11-20 09:02:38 -0600

    Prepping for smoother #week-13 demo...
    Since Angular changed and now throws an error not knowing what a QuizFromWeb is...
    
    i-o-g

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { QuizService } from './quiz.service';
+import { QuizService, QuizFromWeb } from './quiz.service';
 
 interface QuizDisplay {
   quizName: string;

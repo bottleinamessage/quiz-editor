af7fe0bcf3e82ac00be0b92921caa34c91fe2f89
Anthony Rice <arice@madisoncollege.edu>
2022-11-27 07:51:10 -0600

    Using the non-deprecated version of subscribe...

--- src/app/quiz.service.ts
+++ src/app/quiz.service.ts
@@ -20,7 +20,7 @@ export class QuizService {
   loadQuizzes = () => {
 
     const quizzesFromWeb = this.angularHttpClient.get<QuizFromWeb[]>(
-      "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Anthony%20Rice"
+      "https://modern-js.azurewebsites.net/api/HttpTriggerJS1?code=8XD3vN3ehHLdZacBQJQhgUnNst9202gdd5VM3kWCytDkz2nXhia6kA==&name=Mystery%20Quiz"
     );
 
     return quizzesFromWeb;

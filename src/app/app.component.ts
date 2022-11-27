198488b961fed18d1636af0b23977b3b40da5827
Anthony Rice <arice@madisoncollege.edu>
2022-11-27 07:32:16 -0600

    Got quizzes showing...
    What if the HTTP end point fails...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,23 @@ export class AppComponent implements OnInit {
     const quizzes = this.quizSvc.loadQuizzes();
     console.log(quizzes);
 
-    this.quizzes = quizzes.map(x => ({
+    quizzes.subscribe(
+      data => {
+        console.log(data);
+        this.quizzes = data.map(x => ({
+          quizName: x.name
+          , quizQuestions: x.questions.map(y => ({
+            questionName: y.name
+          }))
+          , markedForDelete: false
+        }));
+      }
+      , err => {
+        console.error(err);
+      }
+    );
+    
+/*     this.quizzes = quizzes.map(x => ({
       quizName: x.name
       , quizQuestions: x.questions.map((y: any) => ({
         questionName: y.name
@@ -36,7 +52,7 @@ export class AppComponent implements OnInit {
       , markedForDelete: false
     }));
 
-    console.log(this.quizzes);
+ */    console.log(this.quizzes);
   }
 
   quizzes: QuizDisplay[] = [];
